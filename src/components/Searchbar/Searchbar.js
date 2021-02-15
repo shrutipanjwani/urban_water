b4dddc183283e5e1677c09b46591d4b899ccf845
designersourav <sourav.dhara@gmail.com>
2021-02-15 14:04:30 +0530

    Implemented auto suggest search

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,8 +1,83 @@
 import React, { Component } from 'react'
- import './Searchbar.css'
+import Autosuggest from "react-autosuggest";
+import './Searchbar.css'
+
+const names = [
+    "Brian",
+    "Caley",
+    "Casey",
+    "Caroline",
+    "Chris",
+    "David",
+    "Misha",
+  ];
+  const escapeRegexCharacters = (str) =>
+    str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+  
+  const getSuggestions = (value) => {
+    const escapedValue = escapeRegexCharacters(value.trim());
+  
+    if (escapedValue === "") {
+      return [];
+    }
+  
+    const regex = new RegExp("^" + escapedValue, "i");
+  
+    return names.filter((name) => regex.test(name));
+  };
+  
+  const getSuggestionValue = (suggestion) => suggestion;
+  
+  const renderSuggestion = (suggestion) => suggestion;
+  
+  const renderInputComponent = (inputProps) => (
+    <div className="inputContainer">
+      {/* <img
+        className="icon"
+        src="https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-128.png"
+      /> */}
+      <input {...inputProps} />
+    </div>
+  );
+  
+
+
 //  import image from '../components/images/div_1_image.jpg';
   class Searchbar extends Component {
-     render() {
+    constructor() {
+        super();
+    
+        this.state = {
+          value: "",
+          suggestions: [],
+        };
+      }
+    
+      onChange = (event, { newValue, method }) => {
+        this.setState({
+          value: newValue,
+        });
+      };
+    
+      onSuggestionsFetchRequested = ({ value }) => {
+        this.setState({
+          suggestions: getSuggestions(value),
+        });
+      };
+    
+      onSuggestionsClearRequested = () => {
+        this.setState({
+          suggestions: [],
+        });
+      };
+    
+      render() {
+        const { value, suggestions } = this.state;
+        const inputProps = {
+          placeholder: "Type 'c'",
+          value,
+          onChange: this.onChange,
+        };
          return (
              <div className='container'>
                  {/* <img src={ image } /> */}
@@ -13,16 +88,25 @@ import React, { Component } from 'react'
                  
                  </div> */}
                  <header>
-   <div className='title'>
-     <h1>Simple and reliable maps</h1>
-     <p>We are Water Experts committed to your livelihoods</p>
-     <h2>Flood Risk Maps</h2>
-     <input type="text" className="search-ctrl-geocoder--input" placeholder="Enter Address, Post Code, City" aria-label></input>
-     <p className='para'>
-         Our maps provide flood risk information and flood protection intervention in an easy to use format. Check out our maps below.
-     </p>
-   </div>
- </header>
+                    <div className='title'>
+                        <h1>Simple and reliable maps</h1>
+                        <p>We are Water Experts committed to your livelihoods</p>
+                        <h2>Flood Risk Maps</h2>
+                        <Autosuggest
+                                suggestions={suggestions}
+                                onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+                                onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+                                getSuggestionValue={getSuggestionValue}
+                                renderSuggestion={renderSuggestion}
+                                inputProps={inputProps}
+                                renderInputComponent={renderInputComponent}
+                            />
+                        {/* <input type="text" className="searchCtrlGeocoderInput" placeholder="Enter Address, Post Code, City" aria-label></input> */}
+                        <p className='para'>
+                            Our maps provide flood risk information and flood protection intervention in an easy to use format. Check out our maps below.
+                        </p>
+                    </div>
+                </header>
 
  {/* <main>
    
@@ -33,6 +117,6 @@ import React, { Component } from 'react'
 
              </div>
          );
-     }
+    }
  }
   export default Searchbar
